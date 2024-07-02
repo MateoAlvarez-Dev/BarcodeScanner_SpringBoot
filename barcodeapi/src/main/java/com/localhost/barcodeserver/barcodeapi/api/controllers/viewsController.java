5ad3c78fe2884c5d9be3efc9ef85ca6716325a0e
Juan Pablo <agudelojuanpablo398@gmail.com>
2024-07-02 10:55:42 -0500

    thymeleaf implemented

--- barcodeapi/src/main/java/com/localhost/barcodeserver/barcodeapi/api/controllers/viewsController.java
+++ barcodeapi/src/main/java/com/localhost/barcodeserver/barcodeapi/api/controllers/viewsController.java
@@ -1,6 +1,11 @@
 package com.localhost.barcodeserver.barcodeapi.api.controllers;
 
+import java.net.InetAddress;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,12 +15,30 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/")
 @AllArgsConstructor
 public class viewsController {
+
+    @Autowired
+    Environment environment;
+
+    @GetMapping("/")
+    public String showViewTest(Model model) {
+        String ipAddress = "No se pudo obtener la dirección IP";
+
+        try {
+            InetAddress inetAddress = InetAddress.getLoopbackAddress();
+            ipAddress = inetAddress.getHostAddress();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        model.addAttribute("localAddress", ipAddress);
+
+        return "intro";
+    }
     
     @GetMapping("/scanner")
     public String showViewScanner() {
         return "index";
     }
-
 
     @GetMapping("/receiver")
     public String showViewReceive() {
